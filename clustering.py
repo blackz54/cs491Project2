f56ad0106fd0d63a230d60dd8a3caf9ebabb5684
blackz54 <54559636+blackz54@users.noreply.github.com>
2019-10-18 20:35:28 -0700

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -16,9 +16,34 @@ def K_Means(X, K):
             index = find_closest_center(X[i], centers)
             C[index].append(list(X[i]))
         newCenters = [np.around(np.mean(C[i], axis=0), 2) if len(C[i]) > 0 else 0 for i in range(0, K)]
-        if np.array_equal(centers, newCenters):
+        #newCenters = [around(mean(C[i]), 2) if len(C[i]) > 0 else np.array([0]*len(X[0])) for i in range(0, K)]
+        if array_equal(centers, newCenters):
             return np.array(centers)
         centers = newCenters
+
+
+def mean(C):
+    value = 0
+    groups = []
+    if C is None:
+        return None
+    for i in range(0, len(C[0])):
+        for j in range(0, len(C)):
+            value += C[j][i]
+        value = float(value)/len(C)
+        groups.append(value)
+    return groups
+
+
+def around(X, d):
+    return [round(float(X[i]), 2) for i in range(len(X))]
+
+
+def array_equal(X, Y):
+    for i in range(0, len(X)):
+        if not tuple(X[i]) == tuple(Y[i]):
+            return False
+    return True
 
 
 def initialize_unique_centers(numSamps, K):
